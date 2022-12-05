1e7b8bbd413c81fb2e1cbbdb919a80a5c3b98649
Marlon Nunes <marlon@alfamidia.com.br>
2022-12-05 19:50:57 -0300

    Adicionei verificacao do CPF

--- src/Principal.java
+++ src/Principal.java
@@ -1,15 +1,27 @@
+import java.util.Scanner;
+
 import menu.Menu;
 
 public class Principal {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
 		Menu.bemVindo();
+		
+		String cpf = sc.next();
+		
+		if( !cpf.equals("0") ) {
+			Menu.opcoesCliente();
+			
+			Menu.opcoesAdmin();
+			
+			Menu.opcoesVendedor();			
+		}
 
-		Menu.opcoesCliente();
+
 		
-		Menu.opcoesAdmin();
-		
-		Menu.opcoesVendedor();
 	}
 
 }
