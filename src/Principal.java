85eb39f909ade5c54702b5c75a2b9b5422886829
Marlon Nunes <marlon@alfamidia.com.br>
2022-12-05 21:59:35 -0300

    Finalizando a aula

--- src/Principal.java
+++ src/Principal.java
@@ -1,25 +1,37 @@
 import java.util.Scanner;
 
 import menu.Menu;
+import model.Cliente;
+import service.ClienteService;
 
 public class Principal {
 
 	public static void main(String[] args) {
+		boolean continua = true;
+		Scanner sc = new Scanner(System.in);
+		ClienteService clienteService = new ClienteService(sc);
 		
-		Scanner sc = new Scanner(System.in);		
-		Menu.bemVindo();		
-		String cpf = sc.next();
-		
-		if( !cpf.equals("0") ) {
+		do {
+			Menu.bemVindo();
+			String cpf = sc.next();
+			if(cpf.equals("0")) {
+				break;
+			}
+			
+			Cliente clienteLogado = clienteService.buscarClientePorCpf(cpf);
+			
+			if(clienteLogado != null) {
+				System.out.println("LOGADO!!!");
+			}
+			
 			Menu.opcoesCliente();
-			
+
 			Menu.opcoesAdmin();
-			
-			Menu.opcoesVendedor();			
-		}
 
-
+			Menu.opcoesVendedor();
+		}while(continua);
 		
+
 	}
 
 }
